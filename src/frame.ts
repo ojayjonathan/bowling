c8890a6ced929c1b4d46ef46ba35b75ddf83f5e1
Dennis Ngaburia  Mathenge <dennis.mathenge@griffinglobaltech.com>
2026-01-12 09:46:10 +0300

    updated

--- src/frame.ts
+++ src/frame.ts
@@ -1,56 +1,145 @@
+class Frame {
+  protected frameNumber: number
+  protected rolls: number[] = []
 
-
-class Frame{
-    protected frameNumber: number;
-    protected rolls: number[] = []
-
-  constructor(frameNumber: number){
+  constructor(frameNumber: number) {
     this.frameNumber = frameNumber
   }
-  getRolls(){
-   return []
-  }
-  setRolls(rolls: number[]){
 
+  getRolls() {
+    return [...this.rolls]
   }
 
-  getFrameNumber(){
-
+  setRolls(rolls: number[]) {
+    this.rolls = [...rolls]
   }
 
-    /*
-     * 
-     * @returns true if the roll has a strike, else false
-     */
-    public isStrike(){
-        return false;
-    }
-   /**
-     * 
-     * @returns true if the roll has a spare, else false
-     */
-    public isSpare(){
+  getFrameNumber() {
+    return this.frameNumber
+  }
 
-    }
-    
-    /**
-     * 
-     * @param roll a number representing a roll
-     * @returns true if the roll is valid, i.e does not exceed expected number 
-     * and false if the roll is invalid
-     */
-    public addRoll(roll: number){
-        return false
+  /**
+   * @returns true if the roll has a strike, else false
+   */
+  public isStrike() {
+    return this.rolls.length > 0 && this.rolls[0] === 10
+  }
+
+  /**
+   * @returns true if the roll has a spare, else false
+   */
+  public isSpare() {
+    return (
+      this.rolls.length >= 2 &&
+      this.rolls[0] !== undefined &&
+      this.rolls[1] !== undefined &&
+      this.rolls[0] !== 10 &&
+      this.rolls[0] + this.rolls[1] === 10
+    )
+  }
+
+  /**
+   * @param roll a number representing a roll
+   * @returns true if the roll is valid, i.e does not exceed expected number
+   * and false if the roll is invalid
+   */
+  public addRoll(roll: number) {
+    if (roll < 0 || roll > 10) return false
+
+    // 10th frame rules
+    if (this.frameNumber === 10) {
+      return this.addRollTenthFrame(roll)
     }
 
-    /**
-     * 
-     * @returns true if the rolls in the frame has completed so that we can move to the next frame
-     */
-    public isComplete(){
+    // Frames 1-9
+    return this.addRollNormalFrame(roll)
+  }
 
-        return false
+  /**
+   * @returns true if the rolls in the frame has completed so that we can move to the next frame
+   */
+  public isComplete() {
+    if (this.frameNumber === 10) {
+      if (this.rolls.length < 2) return false
+
+      const first = this.rolls[0]
+      const second = this.rolls[1]
+
+      if (
+        first !== undefined &&
+        second !== undefined &&
+        (first === 10 || first + second === 10)
+      ) {
+        return this.rolls.length === 3
+      }
+
+      return this.rolls.length === 2
     }
+
+    if (this.isStrike()) return true
+    return this.rolls.length === 2
+  }
+
+  
+  private addRollNormalFrame(roll: number) {
+    if (this.rolls.length === 0) {
+      this.rolls.push(roll)
+      return true
+    }
+
+    if (
+      this.rolls.length === 1 &&
+      this.rolls[0] !== undefined &&
+      this.rolls[0] + roll <= 10
+    ) {
+      this.rolls.push(roll)
+      return true
+    }
+
+    return false
+  }
+
+  private addRollTenthFrame(roll: number) {
+    if (this.rolls.length === 0) {
+      this.rolls.push(roll)
+      return true
+    }
+
+    if (this.rolls.length === 1) {
+      if (this.rolls[0] === 10) {
+        this.rolls.push(roll)
+        return true
+      }
+
+      if (
+        this.rolls[0] !== undefined &&
+        this.rolls[0] + roll <= 10
+      ) {
+        this.rolls.push(roll)
+        return true
+      }
+
+      return false
+    }
+
+    if (this.rolls.length === 2) {
+      const first = this.rolls[0]
+      const second = this.rolls[1]
+
+      if (
+        first !== undefined &&
+        second !== undefined &&
+        (first === 10 || first + second === 10)
+      ) {
+        this.rolls.push(roll)
+        return true
+      }
+
+      return false
+    }
+
+    return false
+  }
 }
 
-export default Frame;
+export default Frame
